354cf55cd105552f441f36e628a25cb223a16aaa
Jose Trujillo <jmigueltrujillo@gmail.com>
2018-06-27 12:32:21 -0600

    require cors

--- routes/newEvent.js
+++ routes/newEvent.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const router = express.Router();
+const cors = require('cors');
+const router = express.Router()
+    .use(cors());
 
 const queries = require('../queries');
 
