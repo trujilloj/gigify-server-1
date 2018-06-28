89bc26d2242e646fc2bad980a6b4e12245344646
Jose Trujillo <jmigueltrujillo@gmail.com>
2018-06-28 09:20:42 -0600

    fix cors issues

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = express();
 
 const event = require("./routes/newEvent");
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
