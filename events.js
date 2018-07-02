2cf22aac56f90e055aa86e373946460b896ed36a
Jose Trujillo <jmigueltrujillo@gmail.com>
2018-07-02 09:59:25 -0600

    update seeded data

--- events.js
+++ events.js
@@ -1,13 +1,13 @@
 module.exports = [{
     id: 1,
-    date: "06-30-2018",
+    date: "09-30-2018",
     venue: "Hi-Dive"
 }, {
     id: 2,
-    date: "07-08-2018",
+    date: "10-08-2018",
     venue: "Hi-Dive"
 }, {
     id: 3,
-    date: "08-12-2018",
+    date: "11-12-2018",
     venue: "Hi-Dive"
 }]
